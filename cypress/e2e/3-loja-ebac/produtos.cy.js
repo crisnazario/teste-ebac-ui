4504ce31c6a252cef4047a16267e9f330b1db084
Cristina Nazário <dematosnazariocristina@gmail.com>
2024-02-13 16:27:48 -0300

    Uso de lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+
+describe("Funcionalidade: Produtos", () => {
+
+    beforeEach(() => {
+        cy.visit("http://lojaebac.ebaconline.art.br/produtos/")
+    })
+
+    it.only("Deve selecionar um produto da lista", () => {
+        cy.get('.products > .row')
+            //.first()
+            //.eq(2)
+            .contains("Ajax Full-Zip Sweatshirt")
+            .click()
+
+        cy.get('#tab-title-description > a').should("contain", "Descrição")
+    })
+
+
+
+})
+
